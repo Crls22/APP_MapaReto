37ac0019dcb95e54378b79be5134dd2c5b0d63a6
cice <cice@aula8PovMacN11.local>
2017-02-22 19:11:14 +0100

    // ajustes de desarrollo mejoras en lógica de mapas_220217_ 19:11

--- APP_MapaReto/MAMapalugarFavorito.swift
+++ APP_MapaReto/MAMapalugarFavorito.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MAMapalugarFavorito: UIViewController {
+    
+    
+    // mark: variabls locales
+    
+    var locationManager = CLLocationManager()
 
+    
+    @IBOutlet weak var myMapViewLugaresFavoritos: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+  
+        
+ 
+     // location manager
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    locationManager.delegate = self
+    locationManager.requestWhenInUseAuthorization()
+    locationManager.startUpdatingLocation()
+        
+        
+        let longPressGR = UILongPressGestureRecognizer(target: self, action: #selector(self.actionCreaChincheta(_:)))
+        longPressGR.minimumPressDuration = 2
+        myMapViewLugaresFavoritos.addGestureRecognizer(longPressGR)
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +50,80 @@ class MAMapalugarFavorito: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // mark utils
+    func actionCreaChincheta(_ gesture: UIGestureRecognizer){
+       
+        if gesture.state == UIGestureRecognizerState.began{
+            
+            let puntoTocado = gesture.location(in: myMapViewLugaresFavoritos)
+            let nuevaCoordenada = myMapViewLugaresFavoritos.convert(puntoTocado, toCoordinateFrom: myMapViewLugaresFavoritos)
+            let customLocation = CLLocation(latitude: nuevaCoordenada.latitude, longitude: nuevaCoordenada.longitude)
+            
+            
+            
+            CLGeocoder().reverseGeocodeLocation(customLocation) {
+                (placemarks, error) in
+                var calle = ""
+                var numero = ""
+                var customTitle = ""
+                
+                if let customPlacemarks = placemarks?[0]{
+                    if customPlacemarks.thoroughfare != nil{
+                        calle = customPlacemarks.thoroughfare!
+                    }
+                    if customPlacemarks.subThoroughfare != nil{
+                        numero = customPlacemarks.subThoroughfare!
+                    }
+                    customTitle = "\(calle) \(numero)"
+                    
+                }
+                if customTitle == ""{
+                    customTitle = "Punto añadido el \(Date())"
+                    
+                }
+                
+                // creamos la anotacion
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = nuevaCoordenada
+                annotation.title = customTitle
+                self.myMapViewLugaresFavoritos.addAnnotation(annotation)
+                
+                // guardamos en nuestro array de diccionarios
+                
+                customLugares.append(["name": customTitle,
+                                      "lat": "\(nuevaCoordenada.latitude)",
+                                      "long": "\(nuevaCoordenada.longitude)"])
+                
+                
+                
+            }
+        }
+   
+        
     }
-    */
-
 }
+
+            
+
+
+
+extension MAMapalugarFavorito : CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation = locations[0]
+        let latitud = userLocation.coordinate.latitude
+        let longitud = userLocation.coordinate.longitude
+        
+        let locationData = CLLocationCoordinate2D(latitude: latitud, longitude: longitud)
+        let region = MKCoordinateRegion(center: locationData, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        myMapViewLugaresFavoritos.setRegion(region, animated: true)
+        
+        
+        
+    }
+    
+    
+    
+}
+
+
